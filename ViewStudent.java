a119746c4985466227680c4b1966d164453a13d0
buckbeak99 <asifmesbah59@gmail.com>
2021-06-28 01:27:04 +0600

    view

--- ViewStudent.java
+++ ViewStudent.java
@@ -2,7 +2,20 @@ import java.util.ArrayList;
 
 public class ViewStudent {
 
-    public static void showDetails(ArrayList<Student> studentList, int i) {
+    public void showDetails(ArrayList<Student>studentList){
+        if(studentList.isEmpty()){
+            System.out.println("No Student found. \n");
+            return;
+        }else{
+        System.out.println("---------------------------------------------------------------------------------");
+        System.out.printf("%-18s%-15s%-15s%-20s%-15s%-15s\n" , "Student", "Class" , "Subject", "Average", "No. of days", "Total" );   
+        System.out.printf("%-18s%-15s%-15s%-20s%-15s%-15s\n" , "Name", "" , "List", "Marks", "taught", "earnings"); 
+        System.out.println();
+        for(Student student: studentList){
+            student.showStudent();
+        }
+        System.out.println("---------------------------------------------------------------------------------");
+    }
     }
     
 }
